a858a1fa4108c92f0c0e146191d23fd910ce1769
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2020-12-28 11:36:33 +0100

    SONARJAVA-3614 S6073: Get sample to compilable state
    Still cannot be part of test resources as it requires external
    dependencies.

--- java-checks-test-sources/src/main/files/non-compiling/checks/MockitoArgumentMatchersUsedOnAllParameters.java
+++ java-checks-test-sources/src/main/files/non-compiling/checks/MockitoArgumentMatchersUsedOnAllParameters.java
@@ -1,14 +1,43 @@
 package checks;
 
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.intThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class MockitoArgumentMatchersUsedOnAllParameters {
+  static class Foo {
+    Object bar(int a, Object b, Object c) {
+      return null;
+    }
+
+    String baz(Object a, Object b) {
+      return "hi";
+    }
+
+    void quux(Object a, Object b) {
+    }
+  }
+
+  @Test
   public void nonCompliant() {
+    Foo foo = new Foo();
+    Integer i1 = null, i2 = null, val1 = null, val2 = null;
     given(foo.bar(anyInt(), i1, i2)).willReturn(null); // Noncompliant
     when(foo.baz(eq(val1), val2)).thenReturn("hi"); // Noncompliant
     doThrow(new RuntimeException()).when(foo).quux(intThat(x -> x >= 42), -1); // Noncompliant
     verify(foo).bar(i1, anyInt(), i2); // Noncompliant
   }
 
+  @Test
   public void compliant() {
+    Foo foo = new Foo();
+    Integer i1 = null, i2 = null, val1 = null, val2 = null;
     given(foo.bar(anyInt(), eq(i1), eq(i2))).willReturn(null);
     when(foo.baz(val1, val2)).thenReturn("hi");
     doThrow(new RuntimeException()).when(foo).quux(intThat(x -> x >= 42), eq(-1));
